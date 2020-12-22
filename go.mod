a41c59ce626b940c170b63af82f14ebc5d1c6d4a
fcou <lyz672673107@gmail.com>
2020-12-22 09:09:22 +0800

    kartos

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/marusama/cyclicbarrier v1.1.0
 	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5
 	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
+	google.golang.org/protobuf v1.25.0 // indirect
 )
---
+++ kratos/errors.go
@@ -0,0 +1,69 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	// UnknownReason is unknown reason for error info.
+	UnknownReason = ""
+	// SupportPackageIsVersion1 this constant should not be referenced by any other code.
+	SupportPackageIsVersion1 = true
+)
+
+var _ error = &StatusError{} // _ 用来判断 type StatusError是否实现了error,用作类型断言，如果StatusError没有实现接口error，则编译错误.
+
+// StatusError contains an error response from the server.
+type StatusError struct {
+	// Code is the gRPC response status code and will always be populated.
+	Code int32 `json:"code"`
+	// Message is the server response message and is only populated when
+	// explicitly referenced by the JSON server response.
+	Message string `json:"message"`
+	// Details provide more context to an error.
+	Details []proto.Message `json:"details"`
+}
+
+// WithDetails provided details messages appended to the errors.
+func (e *StatusError) WithDetails(details ...proto.Message) {
+	e.Details = append(e.Details, details...)
+}
+
+// Is matches each error in the chain with the target value.
+func (e *StatusError) Is(target error) bool {
+	err, ok := target.(*StatusError)
+	if ok {
+		return e.Code == err.Code
+	}
+	return false
+}
+
+func (e *StatusError) Error() string {
+	return fmt.Sprintf("error: code = %d desc = %s details = %+v", e.Code, e.Message, e.Details)
+}
+
+// Error returns a Status representing c and msg.
+func Error(code int32, message string, details ...proto.Message) error {
+	return &StatusError{Code: code, Message: message, Details: details}
+}
+
+// Errorf returns New(c, fmt.Sprintf(format, a...)).
+func Errorf(code int32, format string, a ...interface{}) error {
+	return Error(code, fmt.Sprintf(format, a...))
+}
+
+// Reason returns the gRPC status for a particular error.
+// It supports wrapped errors.
+func Reason(err error) *ErrorItem {
+	if se := new(StatusError); errors.As(err, &se) {
+		for _, d := range se.Details {
+			if e, ok := d.(*ErrorItem); ok {
+				return e
+			}
+		}
+	}
+	return &ErrorItem{Reason: UnknownReason}
+}
